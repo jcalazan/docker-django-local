87fdacae259e6126ed66b54604d9afbe59533a94
jonathan <jonathan@calazan.com>
2014-09-03 00:17:56 -0400

    Added Dockerfiles for base, postgresql, and django.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 docker-django-local
 ===================
 
-Dockerfiles to create images for use in local Django development environments
+Dockerfiles to create images for use in local Django development environments.
+
+Designed to be used with [https://github.com/docker/fig](Fig), it will make things a lot easier.
+
+NOTE: Work in progress, more info coming soon.
---
+++ postgresql/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+POSTGRESQL_USER=${POSTGRESQL_USER:-"docker"}
+POSTGRESQL_PASSWORD=${POSTGRESQL_PASSWORD:-"docker"}
+POSTGRESQL_DB=${POSTGRESQL_DB:-"docker"}
+POSTGRESQL_TEMPLATE=${POSTGRESQL_TEMPLATE:-"DEFAULT"}
+
+POSTGRESQL_BIN=/usr/lib/postgresql/9.1/bin/postgres
+POSTGRESQL_CONFIG_FILE=/etc/postgresql/9.1/main/postgresql.conf
+POSTGRESQL_DATA=/var/lib/postgresql/9.1/main
+
+POSTGRESQL_SINGLE="sudo -u postgres $POSTGRESQL_BIN --single --config-file=$POSTGRESQL_CONFIG_FILE"
+
+if [ ! -d $POSTGRESQL_DATA ]; then
+    mkdir -p $POSTGRESQL_DATA
+    chown -R postgres:postgres $POSTGRESQL_DATA
+    sudo -u postgres /usr/lib/postgresql/9.1/bin/initdb -D $POSTGRESQL_DATA -E 'UTF-8'
+    ln -s /etc/ssl/certs/ssl-cert-snakeoil.pem $POSTGRESQL_DATA/server.crt
+    ln -s /etc/ssl/private/ssl-cert-snakeoil.key $POSTGRESQL_DATA/server.key
+fi
+
+$POSTGRESQL_SINGLE <<< "CREATE USER $POSTGRESQL_USER WITH SUPERUSER;" > /dev/null
+$POSTGRESQL_SINGLE <<< "ALTER USER $POSTGRESQL_USER WITH PASSWORD '$POSTGRESQL_PASSWORD';" > /dev/null
+$POSTGRESQL_SINGLE <<< "CREATE DATABASE $POSTGRESQL_DB OWNER $POSTGRESQL_USER TEMPLATE $POSTGRESQL_TEMPLATE;" > /dev/null
+
+exec sudo -u postgres $POSTGRESQL_BIN --config-file=$POSTGRESQL_CONFIG_FILE
